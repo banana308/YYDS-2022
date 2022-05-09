3c1479e91d47e1cadfe583f2c9bd212a4ec544ee
duxin <banbana308@gmail.com>
2022-05-09 14:20:29 +0700

    优化

--- YgLibrary/Python/test1.py
+++ YgLibrary/Python/test1.py
@@ -2,4 +2,7 @@ import pytest
 import allure
 
 def test_passing():
-    assert (1, 2, 3) == (1, 2, 3)
+    allure.dynamic.title(554564156456)
+    with allure.step(11255664556):
+        assert (1, 2, 3) == (1, 2, 3)
+    print("测试成功日志")
