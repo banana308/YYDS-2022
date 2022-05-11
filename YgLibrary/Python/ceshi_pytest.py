c21d783cd186de4ef3c051d2138c63957e6ed344
duxin <banbana308@gmail.com>
2022-05-11 12:32:58 +0700

    优化

--- YgLibrary/Python/ceshi_pytest.py
+++ YgLibrary/Python/ceshi_pytest.py
@@ -6,24 +6,46 @@ import openpyxl
 
 
 
-def test_01():
-    global toten,name
-    name="y1y2y3y4a1"
-    # name=num
-    # name = "fceshi0" + str(int(i))
-    url="http://192.168.10.120:6210/creditUser/creditUserLogIn"
-    headers01 = {'content-type': 'application/json'}
-    data ={
-    "userName":name,
-    "password":"991444c1f732105f81fa51df09c2619d",
-    "loginUrl":"http://192.168.10.120:96"
-    }
-    response = requests.post(url=url, headers=headers01, json=data)
-    # 返回结果json转化
-    results = json.loads(response.text)
-    #print(results)
-    code=results['data']['code']
-    message=results['data']['message']
-    print(code,message)
 
-    assert code == 0
+
+
+
+
+
+for i in range(2,4):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def test_01():
+#     global toten,name
+#     name="y1y2y3y4a1"
+#     # name=num
+#     # name = "fceshi0" + str(int(i))
+#     url="http://192.168.10.120:6210/creditUser/creditUserLogIn"
+#     headers01 = {'content-type': 'application/json'}
+#     data ={
+#     "userName":name,
+#     "password":"991444c1f732105f81fa51df09c2619d",
+#     "loginUrl":"http://192.168.10.120:96"
+#     }
+#     response = requests.post(url=url, headers=headers01, json=data)
+#     # 返回结果json转化
+#     results = json.loads(response.text)
+#     #print(results)
+#     code=results['data']['code']
+#     message=results['data']['message']
+#     print(code,message)
+#
+#     assert code == 0
--- YgLibrary/Python/test1.py
+++ YgLibrary/Python/test1.py
@@ -9,67 +9,76 @@ import openpyxl
 #pytest test1.py -s -q --alluredir=./result --clean-alluredir
 #allure serve ./result
 #C:\python\Scripts
+def book():
+    global workbook,worksheet,worksheet01,URL
+    workbook = openpyxl.load_workbook(r"C:\test\test.xlsx")
+    #print("读取表格成功")
+    shenames = workbook.get_sheet_names()
+    shenames=workbook.sheetnames
+    print("整个表格所有表名:",shenames)  #['各省市', '测试表']
 
-workbook = openpyxl.load_workbook(r"C:\test\test.xlsx")
-#print("读取表格成功")
-shenames = workbook.get_sheet_names()
-shenames=workbook.sheetnames
-print("整个表格所有表名:",shenames)  #['各省市', '测试表']
+    #还可以通过如下写法获得表对象
+    worksheet=workbook[shenames[0]]
+    print("获取所在表的对象：",worksheet)  #<Worksheet "测试表">
 
-#还可以通过如下写法获得表对象
-worksheet=workbook[shenames[0]]
-print("获取所在表的对象：",worksheet)  #<Worksheet "测试表">
+    worksheet01=workbook[shenames[1]]
+    print("获取所在表的对象：",worksheet01)  #<Worksheet "测试表">
 
-worksheet01=workbook[shenames[1]]
-print("获取所在表的对象：",worksheet01)  #<Worksheet "测试表">
+    # name = worksheet.title  # 获取表名
+    # print("获取所在表的表名:",name)  # 各省市
+    # 获取该表相应的行数和列数
+    rows = worksheet.max_row
+    columns = worksheet.max_column
+    print("所在表组成有："+str(rows)+"行",str(columns)+"列")  # 32 13
 
-# name = worksheet.title  # 获取表名
-# print("获取所在表的表名:",name)  # 各省市
-# 获取该表相应的行数和列数
-rows = worksheet.max_row
-columns = worksheet.max_column
-print("所在表组成有："+str(rows)+"行",str(columns)+"列")  # 32 13
+    # 获取该表相应的行数和列数
+    rows01 = worksheet01.max_row
+    columns01 = worksheet01.max_column
+    print("所在表组成有："+str(rows01)+"行",str(columns01)+"列")  # 32 13
 
-# 获取该表相应的行数和列数
-rows01 = worksheet01.max_row
-columns01 = worksheet01.max_column
-print("所在表组成有："+str(rows01)+"行",str(columns01)+"列")  # 32 13
+    # url=worksheet["A2"].value
+    # print(url)
+    URL = worksheet.cell(row=2, column=1).value
+    print(URL)
 
 
-# url=worksheet["A2"].value
-# print(url)
-URL=worksheet.cell(row=2,column=1).value
-print(URL)
+
+
 
 def test_01():
-    allure.dynamic.title(worksheet01.cell(row=2,column=2).value)
-    with allure.step(worksheet01.cell(row=2,column=3).value):
-        url=URL+(worksheet01.cell(row=2,column=5).value)
+    i=2
+    book()
+    allure.dynamic.title(worksheet01.cell(row=i, column=2).value)
+    with allure.step(worksheet01.cell(row=i, column=3).value):
+        url = URL + (worksheet01.cell(row=i, column=5).value)
         header = {'content-type': 'application/json'}
-        para=worksheet01.cell(row=2,column=4).value
+        para = worksheet01.cell(row=i, column=4).value
         # print("请求方式:", para)
-        data=json.loads(worksheet01.cell(row=2,column=6).value)
-        # print("请求header参数：",data)
-        if para=="POST":
-            response=requests.post(url=url, headers=header, json=data)
+        data = json.loads(worksheet01.cell(row=i, column=6).value)
+        print("请求header参数：", data)
+        if para == "POST":
+            response = requests.post(url=url, headers=header, json=data)
         else:
-            response=requests.get(url=url,headers=header,params=data)
-        #响应的json数据转换为可被python识别的数据类型
+            response = requests.get(url=url, headers=header, params=data)
+        # 响应的json数据转换为可被python识别的数据类型
         results = json.loads(response.text)
         # print(results)
-        code=results['data']['code']
-        worksheet01.cell(row=2, column=7).value = str(results)
-        workbook.save
+        code = results['data']['code']
+        worksheet01.cell(row=i, column=7).value = str(results)
+        workbook.save("C:\\test\\test.xlsx")
+        workbook.close()
         assert code == 0
 
 def test_02():
-    allure.dynamic.title(worksheet01.cell(row=3, column=2).value)
-    with allure.step(worksheet01.cell(row=3, column=3).value):
-        url = URL + (worksheet01.cell(row=2, column=5).value)
+    i = 3
+    book()
+    allure.dynamic.title(worksheet01.cell(row=i, column=2).value)
+    with allure.step(worksheet01.cell(row=i, column=3).value):
+        url = URL + (worksheet01.cell(row=i, column=5).value)
         header = {'content-type': 'application/json'}
-        para = worksheet01.cell(row=2, column=4).value
+        para = worksheet01.cell(row=i, column=4).value
         # print("请求方式:", para)
-        data = json.loads(worksheet01.cell(row=2, column=6).value)
+        data = json.loads(worksheet01.cell(row=i, column=6).value)
         # print("请求header参数：",data)
         if para == "POST":
             response = requests.post(url=url, headers=header, json=data)
@@ -79,9 +88,19 @@ def test_02():
         results = json.loads(response.text)
         # print(results)
         code = results['data']['code']
-        worksheet01.cell(row=3, column=7).value = str(results)
-        workbook.save
+        worksheet01.cell(row=i, column=7).value = str(results)
+        workbook.save("C:\\test\\test.xlsx")
+        workbook.close()
         assert code == 0
+
+
+
+
+
+
+
+
+
 
 
 
